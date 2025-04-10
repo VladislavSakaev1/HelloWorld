d3eb06d3b650aa25818b6f546a494e0eac274e7e
VladislavSakaev1 <sakaiev57@mail.ru>
2025-04-10 10:28:23 +0300

    Commit4

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Sakaev Vladislav Aleksandrovich, TRPO");
+        System.out.println("Sakaev Vladislav Aleksandrovich, TRPO.");
     }
 }
