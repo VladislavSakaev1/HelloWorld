a9f039e4cb90658988a2b82f007370cba91278ed
VladislavSakaev1 <sakaiev57@mail.ru>
2025-04-10 09:58:06 +0300

    Commit2

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Сакаев Владислав Александрович, Технология разработки программного обеспечения");
     }
 }
